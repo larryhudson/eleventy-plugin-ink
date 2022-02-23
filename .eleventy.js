b30a61fb1da458c7d9877348c45453516cf616f5
Larry Hudson <larryhudson@hey.com>
2022-02-24 09:14:39 +1100

    got it working

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,74 @@
-const { spawn } = require('child_process');
+const { spawn } = require("child_process");
+const path = require("path");
+const fs = require("fs");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addTemplateFormats("ink");
 
-    eleventyConfig.addTemplateFormats("ink");
+  //   JSON files get written to this directory
+  const workingDirForInkCompiling = "./ink-tmp";
 
-    const workingDirForInkCompiling = './ink-tmp'
+  //   before Eleventy builds, it makes sure that tmp folder exists.
+  //   docs: https://www.11ty.dev/docs/events/
+  eleventyConfig.on("eleventy.before", function () {
+    if (!fs.existsSync(workingDirForInkCompiling)) {
+      fs.mkdirSync(workingDirForInkCompiling);
+    }
+  });
 
-    eleventyConfig.addExtension('ink', {
-        compile: function(str, inputPath) {
-            return async (data) => {
+  // this extension means that eleventy will treat any files in the input directory as input
+  // docs: https://www.11ty.dev/docs/languages/custom/
+  eleventyConfig.addExtension("ink", {
+    outputFileExtension: "json",
+    compile: function (str, inputPath) {
+      return async (data) => {
+        const inkFilename = path.basename(inputPath);
+        const outputPath = path.join(
+          workingDirForInkCompiling,
+          `${inkFilename}.json`
+        );
 
-                // let's see if we can get the ink to convert to JSON and access it in here...
-                console.log({inputPath})
+        // returns the rendered data
+        return await new Promise((resolve) => {
+          //   this executes the ink-tools CLI to compile the input file to the output
+          const inkToolsCommand = spawn("ink-tools", [
+            "compile",
+            inputPath,
+            "--output",
+            outputPath,
+          ]);
 
-                console.log(process.env.PATH)
+          //   Uncomment these lines if you want to see what the ink-tools command is doing
+          //   inkToolsCommand.stdout.on("data", (data) => {
+          //     console.log(`stdout: ${data}`);
+          //   });
 
-                const inkToolsCommand = spawn('ink-tools', ['compile', inputPath], {
-                    env: process.env
-                })
+          //   inkToolsCommand.stderr.on("data", (data) => {
+          //     console.log(`stderr: ${data}`);
+          //   });
 
-                }
-        },
-        
-    })
+          //   when the ink-tools command is finished, the promise is resolved with the data from the text file
+          inkToolsCommand.on("close", (code) => {
+            // we read the file that was just generated
+            fs.readFile(
+              outputPath,
+              {
+                encoding: "utf-8",
+              },
+              function (err, data) {
+                //   resolve the promise with the data from the compiled text file
+                resolve(data);
+              }
+            );
+          });
+        });
+      };
+    },
+  });
+
+  return {
+    dir: {
+      input: "./src",
+    },
   };
+};
