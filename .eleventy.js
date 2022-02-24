c2c6146ab6e75052654641ed5bf417646ea8334d
Larry Hudson <larryhudson@hey.com>
2022-02-25 09:52:53 +1100

    better error handling

--- .eleventy.js
+++ .eleventy.js
@@ -20,6 +20,13 @@ module.exports = function (eleventyConfig, suppliedOptions = {}) {
     // This needs to write a temporary .ink file with the file contents
     // Then convert that .ink file to .json
     // Then return the contents of that rendered JSON file.
+    const temporaryCompilationFolderIncludesSpace =
+      temporaryCompilationFolder.indexOf(" ") !== -1;
+
+    if (temporaryCompilationFolderIncludesSpace) {
+      const spaceErrorMessage = `[eleventy-plugin-ink] temporaryCompilationFolder must not have a space in it`;
+      throw new Error(spaceErrorMessage);
+    }
 
     if (!fs.existsSync(temporaryCompilationFolder)) {
       fs.mkdirSync(temporaryCompilationFolder);
@@ -32,40 +39,55 @@ module.exports = function (eleventyConfig, suppliedOptions = {}) {
       `${fileHash}.json`
     );
 
-    // write the file contents to the 'input path' for ink-tools
-    await fs.promises.writeFile(inputPath, fileContents, {
-      encoding: "utf-8",
-    });
-
-    // returns the rendered data
-    return await new Promise((resolve) => {
-      //   this executes the ink-tools CLI to compile the input file to the output
-      const inkToolsCommand = spawn("ink-tools", ["compile", inputPath]);
-
-      //   Uncomment these lines if you want to see what the ink-tools command is doing
-      // inkToolsCommand.stdout.on("data", (data) => {
-      //   console.log(`stdout: ${data}`);
-      // });
-
-      // inkToolsCommand.stderr.on("data", (data) => {
-      //   console.log(`stderr: ${data}`);
-      // });
-
-      //   when the ink-tools command is finished, the promise is resolved with the data from the text file
-      inkToolsCommand.on("close", (code) => {
-        // we read the file that was just generated
-        fs.readFile(
-          outputPath,
-          {
-            encoding: "utf8",
-          },
-          function (err, data) {
-            //   resolve the promise with the data from the compiled text file
-            resolve(JSON.parse(data.trim()));
-          }
-        );
+    try {
+      // write the file contents to the 'input path' for ink-tools
+      await fs.promises.writeFile(inputPath, fileContents, {
+        encoding: "utf-8",
       });
-    });
+    } catch (e) {
+      console.error(
+        "[eleventy-plugin-ink] Failed to write temporary JSON file"
+      );
+      throw new Error(e);
+    }
+
+    try {
+      // returns the rendered data
+      return await new Promise((resolve, reject) => {
+        //   this executes the ink-tools CLI to compile the input file to the output
+        const inkToolsCommand = spawn("ink-tools", ["compile", inputPath]);
+
+        //   Uncomment these lines if you want to see what the ink-tools command is doing
+        // inkToolsCommand.stdout.on("data", (data) => {
+        //   console.log(`stdout: ${data}`);
+        // });
+
+        inkToolsCommand.stderr.on("data", (data) => {
+          console.error(
+            `[eleventy-plugin-ink] error from ink-tools command:${data}`
+          );
+          throw new Error(data);
+        });
+
+        //   when the ink-tools command is finished, the promise is resolved with the data from the text file
+        inkToolsCommand.on("close", (code) => {
+          // we read the file that was just generated
+          fs.readFile(
+            outputPath,
+            {
+              encoding: "utf8",
+            },
+            function (err, data) {
+              if (err) throw err;
+              resolve(JSON.parse(data.trim()));
+            }
+          );
+        });
+      });
+    } catch (e) {
+      console.error(`[eleventy-plugin-ink] Could not parse the rendered JSON`);
+      throw e;
+    }
   });
 
   // This deletes the temporary folder when it finishes
